7808666735b17aada4cb54f511792b6d90ecd2f2
ljhun1201 <1201ljhun@gmail.com>
2024-11-13 22:44:37 +0900

    Initial commit

--- user-login-service/app.py
+++ user-login-service/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
@@ -14,10 +16,16 @@ class User(db.Model):
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
+    if not data or 'username' not in data or 'password' not in data:
+        return jsonify({"message": "Missing username or password"}), 400
+
     user = User.query.filter_by(username=data['username']).first()
-    if user and user.password == data['password']:
+    if user and check_password_hash(user.password, data['password']):
         return jsonify({"message": "Login successful"}), 200
+
     return jsonify({"message": "Invalid credentials"}), 401
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # 데이터베이스와 테이블 초기화
     app.run(host='0.0.0.0', port=5000)
