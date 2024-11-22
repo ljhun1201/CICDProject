53a37bde39e47a1772043b6e11c2edd06f5a03e7
ljhun1201 <1201ljhun@gmail.com>
2024-11-23 05:23:54 +0900

    ECR4

--- terraform/main.tf
+++ terraform/main.tf
@@ -200,7 +200,7 @@ resource "aws_route53_record" "frontend_alias1" {
     evaluate_target_health = false                    # 대상 상태 평가 여부 (false로 설정)
   }
 }
-/*
+
 resource "aws_ecr_repository" "user_registration_repo" {
   name = "user-registration-service"
   image_scanning_configuration {
@@ -220,4 +220,402 @@ resource "aws_ecr_repository" "user_login_repo" {
     encryption_type = "AES256"
   }
 }
+
+# -----------------------------------------------------------
+# VPC 생성
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "eks-vpc"
+  }
+}
+
+# 퍼블릭 서브넷 생성 (ALB용)
+resource "aws_subnet" "public_subnets" {
+  count                   = 2
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, count.index)
+  availability_zone       = element(["ap-northeast-2a", "ap-northeast-2c"], count.index)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "eks-public-subnet-${count.index}"
+  }
+}
+
+# 프라이빗 서브넷 생성 (EKS용)
+resource "aws_subnet" "private_subnets" {
+  count                   = 2
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.eks_vpc.cidr_block, 8, count.index + 2)
+  availability_zone       = element(["ap-northeast-2a", "ap-northeast-2c"], count.index)
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "eks-private-subnet-${count.index}"
+  }
+}
+
+# 인터넷 게이트웨이 생성
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = {
+    Name = "eks-igw"
+  }
+}
+
+# 퍼블릭 라우팅 테이블 생성 및 연결
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.eks_igw.id
+  }
+
+  tags = {
+    Name = "eks-public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_associations" {
+  count          = length(aws_subnet.public_subnets[*].id)
+  subnet_id      = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_eip" "nat" {
+  vpc = true
+
+  tags = {
+    Name = "eks-nat-eip"
+  }
+}
+
+# NAT 게이트웨이 생성 (프라이빗 서브넷의 인터넷 액세스용)
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = element(aws_subnet.public_subnets[*].id, 0)
+
+  tags = {
+    Name = "eks-nat-gateway"
+  }
+}
+
+# 프라이빗 라우팅 테이블 생성 및 연결
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+
+  tags = {
+    Name = "eks-private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_associations" {
+  count          = length(aws_subnet.private_subnets[*].id)
+  subnet_id      = element(aws_subnet.private_subnets[*].id, count.index)
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+# ALB 보안 그룹 생성
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
+
+# EKS 노드 그룹용 보안 그룹 생성
+resource "aws_security_group" "eks_node_sg" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-node-sg"
+  }
+}
+
+#----------------------------------------------------------------------------
+
+# EKS 클러스터
+resource "aws_eks_cluster" "eks_cluster" {
+  name     = "eks-cluster"
+  role_arn = "arn:aws:iam::481665107235:role/eks-cluster-role"
+
+  vpc_config {
+    subnet_ids = concat(
+      aws_subnet.private_subnets[*].id
+    )
+  }
+
+  tags = {
+    Name = "eks-cluster"
+  }
+}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks_auth.token
+  config_path = "~/.kube/config"
+}
+
+data "aws_eks_cluster_auth" "eks_auth" {
+  name = aws_eks_cluster.eks_cluster.name
+}
+
+# EKS Node Group
+resource "aws_eks_node_group" "eks_node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "eks-node-group"
+  node_role_arn   = "arn:aws:iam::481665107235:role/eks-node-role"
+  subnet_ids      = aws_subnet.private_subnets[*].id
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  tags = {
+    Name = "eks-node-group"
+  }
+}
+
+#--------------------------------------------------------------------------------
+
+resource "kubernetes_deployment" "app_one" {
+  metadata {
+    name      = "app-one"
+    namespace = "default"
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "app-one"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "app-one"
+        }
+      }
+
+      spec {
+        container {
+          name  = "app-one-container"
+          image = aws_ecr_repository.user_registration_repo.repository_url
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_deployment" "app_two" {
+  metadata {
+    name      = "app-two"
+    namespace = "default"
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        app = "app-two"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "app-two"
+        }
+      }
+
+      spec {
+        container {
+          name  = "app-two-container"
+          image = aws_ecr_repository.user_login_repo.repository_url
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "app_one_service" {
+  metadata {
+    name      = "app-one-service"
+    namespace = "default"
+  }
+
+  spec {
+    selector = {
+      app = "app-one"
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "ClusterIP"
+  }
+}
+
+resource "kubernetes_service" "app_two_service" {
+  metadata {
+    name      = "app-two-service"
+    namespace = "default"
+  }
+
+  spec {
+    selector = {
+      app = "app-two"
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "ClusterIP"
+  }
+}
+
+#-------------------------------------------------------------------
+
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks_auth.token
+  }
+}
+
+# ALB Controller Helm 설치
+resource "helm_release" "alb_controller" {
+  name       = "aws-load-balancer-controller"
+  chart      = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  namespace  = "kube-system"
+
+  set {
+    name  = "clusterName"
+    value = aws_eks_cluster.eks_cluster.name
+  }
+
+  set {
+    name  = "region"
+    value = "ap-northeast-2"
+  }
+
+  set {
+    name  = "vpcId"
+    value = aws_vpc.eks_vpc.id
+  }
+}
+
+/*
+resource "kubernetes_manifest" "app_ingress" {
+  manifest = {
+    apiVersion = "networking.k8s.io/v1"
+    kind       = "Ingress"
+    metadata = {
+      name      = "app-ingress"
+      namespace = "default"
+      annotations = {
+        "kubernetes.io/ingress.class"        = "alb"
+        "alb.ingress.kubernetes.io/scheme"  = "internet-facing"
+        "alb.ingress.kubernetes.io/listen-ports" = "[{\"HTTP\": 80}]"
+        "alb.ingress.kubernetes.io/target-type"  = "ip"
+      }
+    }
+    spec = {
+      ingressClassName = "alb"
+      rules = [
+        {
+          host = "ljhun.shop"
+          http = {
+            paths = [
+              {
+                path     = "/app-one"
+                pathType = "Prefix"
+                backend = {
+                  service = {
+                    name = kubernetes_service.app_one_service.metadata[0].name
+                    port = {
+                      number = 80
+                    }
+                  }
+                }
+              },
+              {
+                path     = "/app-two"
+                pathType = "Prefix"
+                backend = {
+                  service = {
+                    name = kubernetes_service.app_two_service.metadata[0].name
+                    port = {
+                      number = 80
+                    }
+                  }
+                }
+              }
+            ]
+          }
+        }
+      ]
+    }
+  }
+}
 */
+
+
+#-----------------------------------------------------------------
