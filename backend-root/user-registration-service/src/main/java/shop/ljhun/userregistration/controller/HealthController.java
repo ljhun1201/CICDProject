296d31a1695fe988cb876f722024bef9fc8f54a4
ljhun1201 <1201ljhun@gmail.com>
2025-04-17 19:11:58 +0900

    Spring Boot

--- backend-root/user-registration-service/src/main/java/shop/ljhun/userregistration/controller/HealthController.java
+++ backend-root/user-registration-service/src/main/java/shop/ljhun/userregistration/controller/HealthController.java
@@ -1,3 +1,10 @@
+package shop.ljhun.userregistration.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HealthController {
 
